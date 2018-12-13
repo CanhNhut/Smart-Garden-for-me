a719d0888cbce224b813654d37ae42b61c0362ed
CanhNhut <nhutori1@gmail.com>
2018-12-13 22:16:01 +0700

    Smart Garden for me

---
+++ Smart_Garden/Turn_Lamp_Pump.ino
@@ -0,0 +1,19 @@
+void TatDen()
+{
+     digitalWrite(pin_den,LOW);
+}
+ void MoDen()
+ {
+     digitalWrite(pin_den,HIGH);
+ 
+ }
+ void TatMayBom()
+ {
+     digitalWrite(pin_bom,LOW); 
+ 
+ }
+ void MoMayBom()
+ {
+   digitalWrite(pin_bom,HIGH);
+   
+ }
